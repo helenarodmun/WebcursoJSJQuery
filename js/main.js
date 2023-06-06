9661cd5f6a2ed97293997c837f184d5077dfed40
helenarodmun <helenarodmun@gmail.com>
2023-06-06 21:28:39 +0100

    Issue #3

--- js/main.js
+++ js/main.js
@@ -12,30 +12,30 @@ $(document).ready(function () {
 
     var posts = [{
         title: 'Prueba de título',
-        date: 'Publicado el día ' + moment().date()+ ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY") ,       
+        date: 'Publicado el día ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
         content: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt tempus elit nec lacinia. Proin arcu nisl, faucibus non turpis sed, ultricies pharetra felis. Ut a enim elit. Integer lectus quam, feugiat tempus sem non, rutrum mollis elit. Aenean congue nisl ipsum, eget maximus lorem faucibus at. Nullam finibus ipsum id dignissim euismod. Fusce achendrerit diam. In nec erat sem. In sit amet aliquet augue. Sed nec tincidunt magna. Praesent eget libero mollis, pulvinar tellus eu, porta elit. Nunc vehicula erat urna, lobortis convallis quam iaculis sed. Vestibulum ornare, felis at bibendum mollis, nulla magna pretium lorem, in sagittis dolor purus eu tortor. Cras varius malesuada elementum."
     },
     {
         title: 'Prueba de título 2',
-        date: 'Publicado el día ' + moment().date()+ ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY") ,  
+        date: 'Publicado el día ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
         content: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt tempus elit nec lacinia. Proin arcu nisl, faucibus non turpis sed, ultricies pharetra felis. Ut a enim elit. Integer lectus quam, feugiat tempus sem non, rutrum mollis elit. Aenean congue nisl ipsum, eget maximus lorem faucibus at. Nullam finibus ipsum id dignissim euismod. Fusce achendrerit diam. In nec erat sem. In sit amet aliquet augue. Sed nec tincidunt magna. Praesent eget libero mollis, pulvinar tellus eu, porta elit. Nunc vehicula erat urna, lobortis convallis quam iaculis sed. Vestibulum ornare, felis at bibendum mollis, nulla magna pretium lorem, in sagittis dolor purus eu tortor. Cras varius malesuada elementum."
     },
     {
         title: 'Prueba de título 3',
-        date: 'Publicado el día ' + moment().date()+ ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY") ,  
+        date: 'Publicado el día ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
         content: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt tempus elit nec lacinia. Proin arcu nisl, faucibus non turpis sed, ultricies pharetra felis. Ut a enim elit. Integer lectus quam, feugiat tempus sem non, rutrum mollis elit. Aenean congue nisl ipsum, eget maximus lorem faucibus at. Nullam finibus ipsum id dignissim euismod. Fusce achendrerit diam. In nec erat sem. In sit amet aliquet augue. Sed nec tincidunt magna. Praesent eget libero mollis, pulvinar tellus eu, porta elit. Nunc vehicula erat urna, lobortis convallis quam iaculis sed. Vestibulum ornare, felis at bibendum mollis, nulla magna pretium lorem, in sagittis dolor purus eu tortor. Cras varius malesuada elementum."
     },
     {
         title: 'Prueba de título 4',
-        date: 'Publicado el día ' + moment().date()+ ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY") ,  
+        date: 'Publicado el día ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
         content: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt tempus elit nec lacinia. Proin arcu nisl, faucibus non turpis sed, ultricies pharetra felis. Ut a enim elit. Integer lectus quam, feugiat tempus sem non, rutrum mollis elit. Aenean congue nisl ipsum, eget maximus lorem faucibus at. Nullam finibus ipsum id dignissim euismod. Fusce achendrerit diam. In nec erat sem. In sit amet aliquet augue. Sed nec tincidunt magna. Praesent eget libero mollis, pulvinar tellus eu, porta elit. Nunc vehicula erat urna, lobortis convallis quam iaculis sed. Vestibulum ornare, felis at bibendum mollis, nulla magna pretium lorem, in sagittis dolor purus eu tortor. Cras varius malesuada elementum."
     },
     {
         title: 'Prueba de título 5',
-        date: 'Publicado el día ' + moment().date()+ ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY") ,  
+        date: 'Publicado el día ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
         content: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt tempus elit nec lacinia. Proin arcu nisl, faucibus non turpis sed, ultricies pharetra felis. Ut a enim elit. Integer lectus quam, feugiat tempus sem non, rutrum mollis elit. Aenean congue nisl ipsum, eget maximus lorem faucibus at. Nullam finibus ipsum id dignissim euismod. Fusce achendrerit diam. In nec erat sem. In sit amet aliquet augue. Sed nec tincidunt magna. Praesent eget libero mollis, pulvinar tellus eu, porta elit. Nunc vehicula erat urna, lobortis convallis quam iaculis sed. Vestibulum ornare, felis at bibendum mollis, nulla magna pretium lorem, in sagittis dolor purus eu tortor. Cras varius malesuada elementum."
     },];
-    
+
     posts.forEach((item, index) => {
         let post = `
         <article class="post">
@@ -44,8 +44,20 @@ $(document).ready(function () {
         <p>${item.content}
         </p>
         <a href="#" class="button-more">Leer más...</a>
-    </article>`;
+        </article>`;
 
-    $('#posts').append(post);
+        $('#posts').append(post);
+    });
+
+    //Selector de tema
+    let theme = $('#theme')
+    $('#to-green').click(function(){
+        theme.attr('href', './css/green.css')
+    });
+    $('#to-blue').click(function(){
+        theme.attr('href', './css/blue.css')
+    });
+    $('#to-red').click(function(){
+        theme.attr('href', './css/red.css')
     });
 });
