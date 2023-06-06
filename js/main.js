d4241cf0a7671a91037a143ebb6abcafdd723d22
helenarodmun <helenarodmun@gmail.com>
2023-06-06 21:36:06 +0100

    Issue #5

--- js/main.js
+++ js/main.js
@@ -60,4 +60,14 @@ $(document).ready(function () {
     $('#to-red').click(function(){
         theme.attr('href', './css/red.css')
     });
+
+    //Scroll arriba de la web
+
+    $('.subir').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop:0
+        } ,500);
+        return false;
+    })
 });
